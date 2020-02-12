58e8574a0486d0b3482d22c9f1fb19be5897d123
manikanta.panuagnti <manikanta.panuganti@thoughtworks.com>
2020-02-12 09:50:02 +0530

    Display books on the UI

--- src/main/java/com/twu/biblioteca/ListBooks.java
+++ src/main/java/com/twu/biblioteca/ListBooks.java
@@ -3,7 +3,8 @@ package com.twu.biblioteca;
 public class ListBooks implements MenuOption {
     @Override
     public void onSelect(UI ui) {
-
+        Library library = ui.getLibrary();
+        library.getBooks().forEach(book -> ui.displayBooks());
     }
 
     @Override
