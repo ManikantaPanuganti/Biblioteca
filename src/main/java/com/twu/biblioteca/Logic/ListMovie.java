003ed9ca56a78ade4fc2a49a026abf8afb535252
manikanta.panuagnti <manikanta.panuganti@thoughtworks.com>
2020-02-12 19:33:20 +0530

    Add list movie option

--- src/main/java/com/twu/biblioteca/Logic/ListMovie.java
+++ src/main/java/com/twu/biblioteca/Logic/ListMovie.java
@@ -1,4 +1,13 @@
 package com.twu.biblioteca.Logic;
 
-public class ListMovie {
+public class ListMovie implements MenuOption {
+    @Override
+    public void onSelect(LibraryUI libraryUi, Library library) {
+
+    }
+
+    @Override
+    public String description() {
+        return null;
+    }
 }
