9194d9ba044ec0f9317d81e6202c7f294ca46aaa
manikanta.panuagnti <manikanta.panuganti@thoughtworks.com>
2020-02-12 10:02:42 +0530

    Add Menu of menuoptions

---
+++ src/main/java/com/twu/biblioteca/Menu.java
@@ -0,0 +1,12 @@
+package com.twu.biblioteca;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Menu {
+    List<MenuOption> menuOptions = new ArrayList<>();
+
+    Menu() {
+        menuOptions.add(new ListBooks());
+    }
+}
