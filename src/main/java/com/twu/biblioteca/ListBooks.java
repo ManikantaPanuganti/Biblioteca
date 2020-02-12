72bafdcc98cd748e22c06e98a70fb2cf445db4b5
manikanta.panuagnti <manikanta.panuganti@thoughtworks.com>
2020-02-12 09:47:08 +0530

    Add Listbooks which is a menuoption

---
+++ src/main/java/com/twu/biblioteca/ListBooks.java
@@ -0,0 +1,13 @@
+package com.twu.biblioteca;
+
+public class ListBooks implements MenuOption {
+    @Override
+    public void onSelect(UI ui) {
+
+    }
+
+    @Override
+    public String description() {
+        return "ListBooks";
+    }
+}
