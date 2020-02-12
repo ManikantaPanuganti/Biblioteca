725133fdbc1f5b3696f2f0ed789199f0c61e3442
manikanta.panuagnti <manikanta.panuganti@thoughtworks.com>
2020-02-12 12:46:44 +0530

    Checking out test

--- src/test/java/com/twu/biblioteca/CheckOutTest.java
+++ src/test/java/com/twu/biblioteca/CheckOutTest.java
@@ -1,7 +1,23 @@
 package com.twu.biblioteca;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class CheckOutTest {
 
+    @Test
+    public void shouldCheckOutABook() throws UnknownBook {
+        UI ui = mock(UI.class);
+        when(ui.getBookTitle()).thenReturn("BOOK");
+        Library library = mock(Library.class);
+        CheckOut checkOut = new CheckOut();
+        doNothing().when(library).checkOut("BOOK");
+
+
+        checkOut.onSelect(ui, library);
+
+        verify(ui, times(1)).handleSuccessfulCheckOut();
+    }
 }
