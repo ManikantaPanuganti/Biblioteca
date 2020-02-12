f036aa5945e79ae9fb4b557a126036bb1850d08d
manikanta.panuagnti <manikanta.panuganti@thoughtworks.com>
2020-02-12 19:29:33 +0530

    Check out Movie

---
+++ src/main/java/com/twu/biblioteca/Logic/CheckOutMovie.java
@@ -0,0 +1,13 @@
+package com.twu.biblioteca.Logic;
+
+public class CheckOutMovie implements MenuOption {
+    @Override
+    public void onSelect(LibraryUI libraryUi, Library library) {
+
+    }
+
+    @Override
+    public String description() {
+        return null;
+    }
+}
