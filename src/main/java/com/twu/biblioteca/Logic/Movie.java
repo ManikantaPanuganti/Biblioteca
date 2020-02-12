f372977edc7ebbb113d2592ecd1e99ac2673806f
manikanta.panuagnti <manikanta.panuganti@thoughtworks.com>
2020-02-12 17:13:02 +0530

    Change .gitignore :

--- src/main/java/com/twu/biblioteca/Logic/Movie.java
+++ src/main/java/com/twu/biblioteca/Logic/Movie.java
@@ -1,4 +1,19 @@
 package com.twu.biblioteca.Logic;
 
 public class Movie {
+    private final String title;
+    private final int year;
+    private final String director;
+    private final int rating;
+
+    public Movie(String title, int year, String director, int rating) {
+        this.title = title;
+        this.year = year;
+        this.director = director;
+        this.rating = rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
--- src/test/java/com/twu/biblioteca/Logic/MovieTest.java
+++ src/test/java/com/twu/biblioteca/Logic/MovieTest.java
@@ -1,7 +1,14 @@
 package com.twu.biblioteca.Logic;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MovieTest {
+    @Test
+    public void shouldGetAMovieName() {
+        Movie movie = new Movie("Title",2020,"Director",1);
 
+        assertEquals("Title",movie.getTitle());
+    }
 }
