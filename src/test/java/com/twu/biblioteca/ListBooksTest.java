a99ba85f0efe40cf388615cef278f8f65c8103ca
manikanta.panuagnti <manikanta.panuganti@thoughtworks.com>
2020-02-12 10:03:08 +0530

    Add listbooks test

--- src/test/java/com/twu/biblioteca/ListBooksTest.java
+++ src/test/java/com/twu/biblioteca/ListBooksTest.java
@@ -1,5 +1,22 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.twu.biblioteca;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
 
 class ListBooksTest {
 
+    @Test
+    public void shouldDisplayBooks() {
+        UI ui = mock(UI.class);
+        Book book = mock(Book.class);
+        ListBooks listBooks = new ListBooks();
+        when(ui.getLibrary()).thenReturn(new Library(List.of(book)));
+
+        listBooks.onSelect(ui);
+
+        verify(ui,times(1)).displayBooks();
+    }
 }
