c061b892e1eca190b7def117ddaa37c72ac33115
manikanta.panuagnti <manikanta.panuganti@thoughtworks.com>
2020-02-12 12:18:00 +0530

    Add return menuoption

---
+++ src/main/java/com/twu/biblioteca/Return.java
@@ -0,0 +1,13 @@
+package com.twu.biblioteca;
+
+public class Return implements MenuOption {
+    @Override
+    public void onSelect(UI ui, Library library) {
+
+    }
+
+    @Override
+    public String description() {
+        return null;
+    }
+}
