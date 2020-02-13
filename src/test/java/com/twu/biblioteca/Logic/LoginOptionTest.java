407912f07c73aa02dd2a9d7aedb5a16426324938
manikanta.panuagnti <manikanta.panuganti@thoughtworks.com>
2020-02-13 11:42:00 +0530

    Add description

---
+++ src/test/java/com/twu/biblioteca/Logic/LoginOptionTest.java
@@ -0,0 +1,15 @@
+package com.twu.biblioteca.Logic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginOptionTest {
+    @Test
+    public void shouldReturnDescription() {
+        LoginOption loginOption = new  LoginOption();
+
+        assertEquals("Login",loginOption.description());
+    }
+
+}
