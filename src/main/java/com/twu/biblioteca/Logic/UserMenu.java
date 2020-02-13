b18314109fea5cc4ac31c245b8d733a3f14d5969
manikanta.panuagnti <manikanta.panuganti@thoughtworks.com>
2020-02-13 17:23:28 +0530

    Add user specialized menu

--- src/main/java/com/twu/biblioteca/Logic/UserMenu.java
+++ src/main/java/com/twu/biblioteca/Logic/UserMenu.java
@@ -1,4 +1,21 @@
 package com.twu.biblioteca.Logic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserMenu {
+    List<MenuOption> menuOptions = new ArrayList<>();
+
+    public UserMenu() {
+        menuOptions.add(new ListBooks());
+        menuOptions.add(new CheckOutBook());
+        menuOptions.add(new ReturnBook());
+        menuOptions.add(new ListMovie());
+        menuOptions.add(new CheckOutMovie());
+        menuOptions.add(new LogOutOption());
+    }
+
+    public List<MenuOption> getOptions() {
+        return menuOptions;
+    }
 }
